b49d86002fe12adc50ee77c15519c6b55602c351
dipesh-cwd <codewithdipesh44@gmail.com>
2024-12-05 22:01:46 +0545

    improve home page

--- minor-app/src/component/login_page/Login.jsx
+++ minor-app/src/component/login_page/Login.jsx
@@ -5,15 +5,18 @@ function Login() {
  
   
   return (
-    <div className="div">
-      <div className="div1">
-      <div className="close1"></div>
-      </div>
+    // <div className="div">
+    //   <div className="div1">log-in
+    //   <i className="fa-solid fa-xmark fa-xl" style={{color : 'red', height : '50px' , width : '50px',  fontSize: '4em'}}></i> 
 
-      <div className="div2"></div>
-      <i className="fa-solid fa-xmark fa-xl" style={{color : 'red', height : '50px' , width : '50px',  fontSize: '4em'}}></i> 
+    //   <div className="close1"></div>
+    //   </div>
+
+    //   <div className="div2"></div>
+     
       
-    </div>
+    // </div>
+    <></>
   );
 }
 
